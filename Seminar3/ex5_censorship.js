32b5cb158c1cdd164de568024b4f2b5f2dd92d43
gabriel-cristian <85562686+gabriel-cristian@users.noreply.github.com>
2025-10-17 10:14:44 +0300

    To do - ex 5  - Seminar 3
    Implement text censorship function using a dictionary.

---
+++ Seminar3/ex5_censorship.js
@@ -0,0 +1,2 @@
+//implementați cenzurarea unui text printr-o funcție. Funcția primește un șir de caractere și un dicționar sub forma unui array.
+// De exemplu pentru șirul "javascript este minunat" și dicționarul ["este"] funcția va produce "javascript e**e minunat".
