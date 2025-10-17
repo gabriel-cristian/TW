5292539f4379f36e2c5ec51ac08b15f36b9edbd0
gabriel-cristian <85562686+gabriel-cristian@users.noreply.github.com>
2025-10-17 10:10:07 +0300

    To Do - Ex 2 - Seminar 3

---
+++ Seminar3/ex2_filter_reduce.js
@@ -0,0 +1,3 @@
+//TO DO
+// implementați o funcție care primește ca parametrii 
+//un array de numere și un număr și returnează suma tuturor numerelor din array divizibile cu cel de-al doilea parametru.
