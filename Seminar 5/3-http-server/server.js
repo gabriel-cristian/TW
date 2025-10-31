b60e229e5d174ebba41fee6211c0d42459a04501
gabriel-cristian <85562686+gabriel-cristian@users.noreply.github.com>
2025-10-31 09:11:02 +0200

    Initialize Express server with API endpoints
    Set up a basic Express server with CORS and body parsing. Added GET and POST endpoints for managing a list of users.

---
+++ Seminar 5/3-http-server/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+const router = express.Router();
+
+//implementam politica Cors
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+app.use("/api", router);
+
+// Datele inițiale (se pot extinde sau modifica)
+const array = [
+  { id: 1, name: "Ionut", age: 25 },
+  { id: 2, name: "Maria", age: 21 },
+  { id: 3, name: "Eugen", age: 26 },
+  { id: 4, name: "Mara", age: 28 },
+];
+
+// Endpoint GET pentru a prelua lista de elemente
+router.get("/getList", (req, res) => {
+  res.json(array);
+});
+
+// Endpoint POST pentru a adăuga un nou element în listă
+router.post("/postList", (req, res) => {
+  let el = req.body;
+  array.push(el);
+  res.json(el);
+});
+
+app.listen(8000, () => console.log("Server is running on port 8000"));
